9e4570aa45c97354fb1ef0d38092137619a1abf2
Sil silas <silaswans@gmail.com>
2020-07-28 19:27:43 +0300

    Adding position

--- app/src/main/java/com/example/PopulateSpinner/MainActivity.java
+++ app/src/main/java/com/example/PopulateSpinner/MainActivity.java
@@ -3,14 +3,19 @@ package com.example.PopulateSpinner;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -40,5 +45,27 @@ public class MainActivity extends AppCompatActivity {
         ad = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1 ,friends);
 
         lv_listNames.setAdapter(ad);
+
+
+        btn_add.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        String newName = et_addNames.getContext().toString();
+                        friends.add(n);
+
+                        Collections.sort(friends);
+
+                        ad.notifyDataSetChanged();
+                    }
+                }
+        );
+
+        lv_listNames.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Toast.makeText(MainActivity.this, "pos = " + i + "name = " + friends.get(i), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
